35aaad567c83d1cf7485ad3b1201fa0399bbeff9
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-08-03 19:34:54 +0900

    update: add initialization code and remove tree-sitter-c directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use tree_sitter::{Language, Parser};
+
 fn main() {
+    let mut parser = Parser::new();
+    parser
+        .set_language(tree_sitter_c::language())
+        .expect("Error loading C grammar");
     println!("Hello, world!");
 }
