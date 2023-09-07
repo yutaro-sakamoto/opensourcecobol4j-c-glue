302432b3b94ab64f347f262ab7313d0316cc90d6
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-09-08 08:20:09 +0900

    update: CobolRunnableCGlue.java

--- CobolRunnableCGlue.java
+++ CobolRunnableCGlue.java
@@ -1,6 +1,7 @@
 import jp.osscons.opensourcecobol.libcobj.common.*;
 import jp.osscons.opensourcecobol.libcobj.call.*;
 import jp.osscons.opensourcecobol.libcobj.data.*;
+import java.nio.ByteBuffer;
 
 abstract public class CobolRunnableCGlue implements CobolRunnable {
     @Override
@@ -13,18 +14,34 @@ abstract public class CobolRunnableCGlue implements CobolRunnable {
     }
 
     byte[] storageToByteArray(CobolDataStorage storage, int size) {
-        return null;
+        return storage.getByteArray(0, size);
     }
 
     byte storageToByte(CobolDataStorage storage) {
-        return 0;
+        return storage.getByte(0);
     }
 
     short storageToShort(CobolDataStorage storage) {
-        return 0;
+        return ByteBuffer.wrap(storage.getByteArray(0, 2)).getShort();
     }
 
     int storageToInt(CobolDataStorage storage) {
-        return 0;
+        return ByteBuffer.wrap(storage.getByteArray(0, 4)).getInt();
+    }
+
+    void bytesToStorage(CobolDataStorage storage, byte[] bytes) {
+        storage.setBytes(bytes);
+    }
+
+    void byteToStorage(CobolDataStorage storage, byte b) {
+        storage.setByte(b);
+    }
+
+    void shortToStorage(CobolDataStorage storage, short s) {
+        storage.setBytes(ByteBuffer.allocate(2).putShort(s).array());
+    }
+
+    void intToStorage(CobolDataStorage storage, int i) {
+        storage.setBytes(ByteBuffer.allocate(4).putInt(i).array());
     }
 }
